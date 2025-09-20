ac30d2eada15a6d28ba96d721ee85f30ee774ada
TheSilentNights <yangsichen080320@outlook.com>
2025-09-20 20:31:04 +0800

    set up

---
+++ src/main/java/com/thesilentnights/simpleclienttools/Simpleclienttools.java
@@ -0,0 +1,12 @@
+package com.thesilentnights.simpleclienttools;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import net.fabricmc.api.ModInitializer;
+
+public class Simpleclienttools implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/thesilentnights/simpleclienttools/client/SimpleclienttoolsClient.java
@@ -0,0 +1,10 @@
+package com.thesilentnights.simpleclienttools.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleclienttoolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
